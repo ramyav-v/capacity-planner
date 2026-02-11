e47e2e561ed72825c93b6b88d815609fd862f3a3
ramyav-v <ramyav3010@gmail.com>
2026-02-11 22:01:01 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/zinier/capacity_planner/CapacityPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.zinier.capacity_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacityPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacityPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zinier/capacity_planner/CapacityPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinier.capacity_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacityPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
